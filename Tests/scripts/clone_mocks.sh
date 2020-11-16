f9703c34fd277da1d4b14a030f54fb42753e4d57
hod-alpert <haplert@paloaltonetworks.com>
2020-11-16 09:09:04 +0200

    Some build refactories

--- Tests/scripts/clone_mocks.sh
+++ Tests/scripts/clone_mocks.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-keyscan github.com >> ~/.ssh/known_hosts 2>&1
 
 if [[ ! -d "content-test-data" ]]; then
-    git clone git@github.com:demisto/content-test-data.git
+    git clone git@github.com:demisto/content-test-data.git -q
 fi
